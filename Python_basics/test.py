59bf07c7e3ca0af4b495ff84b5e1b187cda179d3
Tejas Chavan <tejschavan2003@gmail.com>
2025-07-18 08:22:43 +0530

    first commit

---
+++ Python_basics/test.py
@@ -0,0 +1,33 @@
+print(1+1)
+
+list = [1 ,2,3,4,5,6,7,8]
+idx = 0
+x = 5
+
+# while idx < len(list):
+#     if list[idx] == x:
+#         idx += 1
+#         continue
+#     print(list[idx])
+#     idx += 1        
+    
+
+while idx < len(list):
+    print(list[idx])
+    if list[idx] == x:
+        break
+    idx += 1
+
+for i in range(1,11,2):
+    print(i)
+
+
+for i in range(0,100,2):
+    print(i) 
+
+
+
+
+
+
+
---
+++ module/package/test.py
@@ -0,0 +1,7 @@
+from maths import * 
+
+# print(addition(1,2))
+# print(substraction(3,8))
+
+from subpackage.mult import mult
+print(mult(1,2))
